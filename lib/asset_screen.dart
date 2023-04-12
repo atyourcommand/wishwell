eb9b50278f02cc926e863770431e618863c90a77
John Brunskill <johnlouisbrunskill@gmail.com>
2023-04-12 16:48:58 +1000

    some testing of json data

--- lib/asset_screen.dart
+++ lib/asset_screen.dart
@@ -1,10 +1,126 @@
 //import 'dart:convert';
+
+//import 'dart:convert';
+
+//import 'dart:convert';
+
 import 'package:flutter/material.dart';
-//import 'package:shared_preferences/shared_preferences.dart';
-//import 'package:wishwell/client_model.dart';
-//import 'package:wishwell/shared_preferences.dart';
-//import 'package:wishwell/client_detail.dart';
-//import 'components/nav2.dart';
+
+class Assets {
+  String? id;
+  String? name;
+  List<Shares>? shares;
+
+  Assets({this.id, this.name, this.shares});
+
+  Assets.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    if (json['shares'] != null) {
+      shares = <Shares>[];
+      json['shares'].forEach((v) {
+        shares!.add(Shares.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    if (shares != null) {
+      data['shares'] = shares!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Shares {
+  String? clientId;
+  int? clientShare;
+
+  Shares({this.clientId, this.clientShare});
+
+  Shares.fromJson(Map<String, dynamic> json) {
+    clientId = json['clientId'];
+    clientShare = json['clientShare'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['clientId'] = clientId;
+    data['clientShare'] = clientShare;
+    return data;
+  }
+}
+
+var jsonData = {
+  "asset": [
+    {
+      "id": "001",
+      "type": "property",
+      "name": "asset name 1",
+      "value": "",
+      "shares": [
+        {"clientId": "1", "clientShare": 20},
+        {"clientId": "2", "clientShare": 40}
+      ],
+      "sole": false
+    },
+    {
+      "id": "002",
+      "type": "cash",
+      "name": "asset name 2",
+      "value": "",
+      "shares": [],
+      "sole": true
+    },
+    {
+      "id": "003",
+      "type": "shares",
+      "name": "asset name 3",
+      "value": "",
+      "shares": [],
+      "sole": false
+    },
+    {
+      "id": "004",
+      "type": "posession",
+      "name": "asset name 4",
+      "value": "",
+      "shares": [
+        {"clientId": "1", "clientShare": 50},
+        {"clientId": "2", "clientShare": 50}
+      ],
+      "sole": false
+    }
+  ]
+}; // your json as string
+
+//final Data _data = Data.fromJson(jsonData);
+
+class Data {
+  List<Assets>? asset;
+
+  Data({this.asset});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    if (jsonData['asset'] != null) {
+      asset = <Assets>[];
+      json['asset'].forEach((v) {
+        asset!.add(Assets.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (asset != null) {
+      data['asset'] = asset!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
 
 class AssetScreen extends StatefulWidget {
   const AssetScreen({Key? key}) : super(key: key);
@@ -22,37 +138,16 @@ class AssetScreenState extends State<AssetScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    body: SafeArea(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              SizedBox(
-                height: 50,
-              ),
-              Text("Assets",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w300,
-                  )),
-              // OutlinedButton(
-              //   onPressed: () {
-              //     Navigator.push(
-              //         context,
-              //         MaterialPageRoute(
-              //             builder: (context) => const Nav2()));
-              //   },
-              //   child: const Text('+ Client'),
-              // ),
-              SizedBox(
-                height: 20,
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: 2,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: Colors.amber,
+            //child: Center(child: Text('Asset: ${_data['']}')),
+          );
+        });
+  }
 }
